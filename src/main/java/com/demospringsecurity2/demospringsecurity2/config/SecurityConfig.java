7af1da4b38de4f12993ab5d0dfc50999bbafbefc
r.ron@gl-f.com <r.ron@gl-f.com>
2018-02-15 11:13:55 +0700

    add basic authenication in memory way 2

--- src/main/java/com/demospringsecurity2/demospringsecurity2/config/SecurityConfig.java
+++ src/main/java/com/demospringsecurity2/demospringsecurity2/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.demospringsecurity2.demospringsecurity2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
@@ -12,26 +14,18 @@ Create By: Ron Rith
 Create Date: 2/15/2018
 */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
-                .and().httpBasic()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+                .antMatchers("/**").hasAnyRole("ADMIN","USER").
+                and().formLogin();
     }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("123456").roles("USER")
-                .and()
-                .withUser("admin").password("123456")
-                .credentialsExpired(true)
-                .accountExpired(true)
-                .accountLocked(true)
-                .authorities("WRITE_PRIVILEGES", "READ_PRIVILEGES")
-                .roles("ADMIN");
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("user123").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("admin123").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("dba").password("dba123").roles("DBA");
     }
 }
