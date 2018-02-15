4e7e769417abb10ec9afff77246642a8bbb33784
r.ron@gl-f.com <r.ron@gl-f.com>
2018-02-15 11:09:38 +0700

    add basic authenication in memory

--- src/main/java/com/demospringsecurity2/demospringsecurity2/domain/HelloController.java
+++ src/main/java/com/demospringsecurity2/demospringsecurity2/domain/HelloController.java
@@ -3,21 +3,18 @@ package com.demospringsecurity2.demospringsecurity2.domain;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /*
 Create By: Ron Rith
 Create Date: 2/15/2018
 */
-@RestController
+@Controller
 public class HelloController {
     private UserDetailsService userService;
 
-    @GetMapping("/")
+    @RequestMapping(value = "/")
     public String greeting(Authentication authentication) {
-        return "Spring Security In-memory Authentication Example";
+        return "hello";
     }
 }
